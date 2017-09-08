a6b0e116af822b4bd7a6e387f3ccf3299ef9c838
RightEdCha <x64marks@ufl.edu>
2017-09-07 20:39:20 -0400

    final version

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,37 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
+
+
 /* Connect to your database */
-
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+mongoose.connect(config, function(err, db) {
+  if (err)
+    return console.log(err);
+});
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
+ Listing.collection.insertMany(fs.readFile('listings.json', 'utf8', function(err, data) {
+    if (err) throw err;
+    listingData = JSON.parse(data);
+ }),
+ function(err,request){
+    assert.equal(null, err);
+    assert.equal(3,request.insertedCount);
 
+    db.close
+ });
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
